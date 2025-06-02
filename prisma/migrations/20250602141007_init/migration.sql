5c9c9953c24dd47d1bc548020e6708248d4a2494
gakfc1406 <gakfc1406@gmail.com>
2025-06-02 23:59:24 +0700

    fix prisma

--- prisma/migrations/20250602141007_init/migration.sql
+++ prisma/migrations/20250602141007_init/migration.sql
@@ -5,6 +5,10 @@ CREATE TABLE "Product" (
     "description" TEXT NOT NULL,
     "price" DECIMAL(65,30) NOT NULL,
     "image" TEXT,
+    "category" TEXT,
+    "stock" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
